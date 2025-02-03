0e30dbfefbe9153fd2655f7cc14edd0da6852099
CrisOrc <cristian.camilo.or05@gmail.com>
2025-02-02 21:42:47 -0500

    Fix all types

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,47 +1,25 @@
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
-import { NextRequest } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { typeDefs } from "@/graphql/typeDef";
 import { resolvers } from "@/graphql/resolvers";
 import { Session } from "@prisma/client";
-
-// Define the GraphQL context type
-interface GraphQLContext {
-  session: Session | null;
-}
+import { prisma } from "@/lib/prisma";
 
 // Create an instance of ApolloServer with the provided type definitions and resolvers
-const server = new ApolloServer<GraphQLContext>({
+const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
 // Handler to start the Apollo Server and create a Next.js handler
-const handler = startServerAndCreateNextHandler<NextRequest, GraphQLContext>(
-  server,
-  {
-    context: async () => {
-      const nextAuthSession = await getServerSession(authOptions);
-      const session: Session | null = nextAuthSession
-        ? {
-            id: nextAuthSession.id,
-            userId: nextAuthSession.userId,
-            sessionToken: nextAuthSession.sessionToken,
-            expires: nextAuthSession.expires.toISOString(),
-          }
-        : null;
-      return { session };
-    },
+const handler = startServerAndCreateNextHandler(server, {
+  context: async () => {
+    const session: Session | null = await getServerSession(authOptions);
+    return { session, prisma };
   },
-);
+});
 
 // Exporting the handler for both GET and POST methods
-export async function POST(req: NextRequest) {
-  return handler(req);
-}
-
-export async function GET(req: NextRequest) {
-  return handler(req);
-}
+export { handler as GET, handler as POST };
